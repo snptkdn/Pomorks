6cf35c650833a8a76c7f73e6e016b904c9d56e67
ryota-kita <400m.wflick@gmail.com>
2022-06-15 10:14:04 +0900

    ライブラリを乗り換えた。

--- pomorks_gui/src/Timer.tsx
+++ pomorks_gui/src/Timer.tsx
@@ -1,26 +1,21 @@
 import React from "react";
-import { useTimer } from "react-timer-hook"
+import { useTimer } from "use-timer"
 import { PomodoroState } from "./pomodoroStatus"
 import { TYPE_STATE } from "./pomodoroStatus"
 import { getTimerSeconds } from "./pomodoroStatus"
 
 export function PomodoroTimer() {
   const state: PomodoroState = new PomodoroState("WORK");
-  const stateStatus = state.getState();
-  const timerSeconds = getTimerSeconds(stateStatus);
-  const expiryTimestamp = new Date();
-  expiryTimestamp.setSeconds(getTimerSeconds(state.getState()));
   const {
-    seconds,
-    minutes,
-    isRunning,
+    time,
     start,
     pause,
-    resume,
-    restart,
+    reset,
+    status
   } = useTimer({
-    expiryTimestamp,
-    onExpire: () => console.warn("onExpire called"),
+    initialTime: getTimerSeconds(state.getState()),
+    timerType: "DECREMENTAL",
+    endTime: 0
   });
 
   function startPomodoro(state: PomodoroState) {
@@ -32,12 +27,11 @@ export function PomodoroTimer() {
     <div style={{ textAlign: "center" }}>
       <h1>Pomorks</h1>
       <div style={{ fontSize: "100px" }}>
-        <span>{minutes}</span>:<span>{seconds}</span>
+        <span>{Math.floor(time/60)}</span>:<span>{time%60}</span>
       </div>
-      <p>{ isRunning ?  "Process..." : "Done!" } </p>
+      <p>{ status === "RUNNING" ? "Process..." : "Done!" } </p>
       <button onClick={() => startPomodoro(state)}>Start</button>
       <button onClick={pause}>Pause</button>
-      <button onClick={resume}>Resume</button>
       <button
         onClick={() => {
           const stateStatus = state.getState();
@@ -45,7 +39,7 @@ export function PomodoroTimer() {
           const time = new Date();
 
           time.setSeconds(time.getSeconds() + timerSeconds) // 25minutes
-          restart(time);
+          reset();
         }}
       >
         Restart
