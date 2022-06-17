f17eb497e02b392d56cc62c40fc6dc6e2b9b3276
ryota-kita <400m.wflick@gmail.com>
2022-06-17 11:50:42 +0900

    TODOにタグとプロジェクトも指定できるようにした。

--- pomorks_gui/src/todo.tsx
+++ pomorks_gui/src/todo.tsx
@@ -3,37 +3,61 @@ import { useState } from 'react';
 import { TextField } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { IconButton } from '@mui/material';
+import { Grid } from '@mui/material';
+import { height } from '@mui/system';
 
 type Todo = {
   title: string;
+  tag: string;
+  project: string;
   readonly id: string;
   checked: boolean;
   removed: boolean;
 };
 
 type Filter = 'all' | 'finished' | 'progress' | 'trash';
+type Attribute = 'title' | 'tag' | 'project';
 
 export function Todo() {
-  const [text, setText] = useState('');
+  const [todo, setValues] = useState({
+    title: '',
+    tag: '',
+    project: '',
+  });
   const [todos, setTodos] = useState<Todo[]>([]);
   const [filter, setFilter] = useState<Filter>('all');
 
   const handleOnSubmit = () => {
-    if (!text) return;
+    if (!todo.title) return;
 
     const newTodo: Todo = {
-      title: text,
+      title: todo.title,
+      tag: todo.tag,
+      project: todo.project,
       id: Math.random().toString(32).substring(2),
       checked: false,
       removed: false,
     };
 
     setTodos([newTodo, ...todos]);
-    setText('');
+    setValues({ ...todo, title: '' });
   };
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setText(e.target.value);
+  const handleOnChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    attr: Attribute,
+  ) => {
+    switch (attr) {
+      case 'title':
+        setValues({ ...todo, title: e.target.value });
+        break;
+      case 'tag':
+        setValues({ ...todo, tag: e.target.value });
+        break;
+      case 'project':
+        setValues({ ...todo, project: e.target.value });
+        break;
+    }
   };
 
   const handleOnEdit = (id: string, value: string) => {
@@ -94,12 +118,6 @@ export function Todo() {
 
   return (
     <div>
-      <select defaultValue="all" onChange={(e) => setFilter(e.target.value as Filter)}>
-        <option value="all">All Task</option>
-        <option value="finished">Finished Task</option>
-        <option value="progress">Progress Task</option>
-        <option value="trash">Trash</option>
-      </select>
       {filter === 'trash' ? (
         <button onClick={() => handleOnEmpty()}>Remove all.</button>
       ) : (
@@ -110,20 +128,49 @@ export function Todo() {
               handleOnSubmit();
             }}
           >
-            <TextField
-              margin="normal"
-              label="NewTask"
-              fullWidth
-              value={text}
-              placeholder="Input New Task"
-              onChange={(e) => handleOnChange(e)}
-            />
-            <IconButton onClick={handleOnSubmit}>
-              <AddIcon></AddIcon>
-            </IconButton>
+            <Grid container>
+              <Grid item xs={11}>
+                <TextField
+                  margin="normal"
+                  label="NewTask"
+                  value={todo.title}
+                  variant="standard"
+                  placeholder="Input New Task"
+                  inputProps={{ style: { fontSize: 10 } }}
+                  onChange={(e) => handleOnChange(e, 'title')}
+                />
+                <TextField
+                  margin="normal"
+                  label="Tag"
+                  value={todo.tag}
+                  variant="standard"
+                  placeholder="Input New Task"
+                  inputProps={{ style: { fontSize: 10 } }}
+                  onChange={(e) => handleOnChange(e, 'tag')}
+                />
+                <TextField
+                  margin="normal"
+                  label="Project"
+                  value={todo.project}
+                  variant="standard"
+                  placeholder="Input New Task"
+                  inputProps={{ style: { fontSize: 10 } }}
+                  onChange={(e) => handleOnChange(e, 'project')}
+                />
+                <IconButton onClick={handleOnSubmit}>
+                  <AddIcon style={{ verticalAlign: 'middle', display: 'inline-flex' }}></AddIcon>
+                </IconButton>
+              </Grid>
+            </Grid>
           </form>
         )
       )}
+      <select defaultValue="all" onChange={(e) => setFilter(e.target.value as Filter)}>
+        <option value="all">All Task</option>
+        <option value="finished">Finished Task</option>
+        <option value="progress">Progress Task</option>
+        <option value="trash">Trash</option>
+      </select>
       <ul>
         {filteredTodos.map((todo) => {
           return (
