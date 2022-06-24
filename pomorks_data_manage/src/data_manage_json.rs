1af59f8bdbd58cab4327684ad1bfeb6baa097c3d
ryota-kita <ryota-kita@kke.co.jp>
2022-06-24 13:28:08 +0900

    タスクファイルが存在しない場合に、処理が落ちる問題を修正した。

--- pomorks_data_manage/src/data_manage_json.rs
+++ pomorks_data_manage/src/data_manage_json.rs
@@ -1,8 +1,10 @@
 use crate::data_manage_trait::DataManage;
 use crate::todo::{self, *};
-use anyhow::Result;
+use anyhow::{Context, Result};
+use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Read, Write};
+use std::path::Path;
 
 pub struct DataManageJson {}
 
@@ -19,8 +21,14 @@ impl DataManage for DataManageJson {
     }
 
     fn read_all_todo() -> Result<Option<todo::TodoList>> {
-        let todo_list_json = File::open("task.json")?;
-        let todo_list: todo::TodoList = serde_json::from_reader(todo_list_json)?;
+        let todo_list_json = match File::open("task.json") {
+            Ok(file) => file,
+            Err(_) => File::create("task.json").context("can't create file.")?,
+        };
+        let todo_list: todo::TodoList = match serde_json::from_reader(todo_list_json) {
+            Ok(todo_list) => todo_list,
+            Err(_) => TodoList::new(),
+        };
 
         Ok(Some(todo_list))
     }
