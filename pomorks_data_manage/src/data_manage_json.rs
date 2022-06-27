f49f0d24810e6731f0329e86cd8e531d193985b9
ryota-kita <ryota-kita@kke.co.jp>
2022-06-27 11:23:23 +0900

    完了済タスクをアーカイブする関数を実装した。

--- pomorks_data_manage/src/data_manage_json.rs
+++ pomorks_data_manage/src/data_manage_json.rs
@@ -2,7 +2,7 @@ use crate::data_manage_trait::DataManage;
 use crate::todo::{self, *};
 use anyhow::{Context, Result};
 use std::env;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, BufReader, Read, Write};
 use std::path::Path;
 
@@ -31,5 +31,25 @@ impl DataManage for DataManageJson {
         };
 
         Ok(Some(todo_list))
+    }
+
+    fn archive_todo(mut archived_todo_list: Vec<TodoItem>) -> Result<()> {
+        let current_archive_json = match File::open("archive.json") {
+            Ok(file) => file,
+            Err(_) => File::create("archive.json")?,
+        };
+        let current_archive = match serde_json::from_reader(current_archive_json) {
+            Ok(archive) => archive,
+            Err(_) => Vec::<TodoItem>::new(),
+        };
+
+        archived_todo_list.extend(current_archive);
+        let serialized = serde_json::to_string(&archived_todo_list)?;
+        // TODO!: ファイル名は引数指定
+        let mut file = File::create("archive.json")?;
+        write!(file, "{}", serialized)?;
+        file.flush()?;
+
+        Ok(())
     }
 }
--- pomorks_data_manage/src/data_manage_trait.rs
+++ pomorks_data_manage/src/data_manage_trait.rs
@@ -4,4 +4,5 @@ use anyhow::Result;
 pub trait DataManage {
     fn write_all_todo(todo_list: TodoList) -> Result<()>;
     fn read_all_todo() -> Result<Option<TodoList>>;
+    fn archive_todo(archived_todo_list: Vec<TodoItem>) -> Result<()>;
 }
